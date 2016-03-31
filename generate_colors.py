9606ed1de1f6b4fa18f8b1f18e745204c803bbf4
jonieva <jorgeonieva@gmail.com>
2016-03-31 16:15:45 -0400

    TG-2 #in-progress ENH: Added colors

--- generate_colors.py
+++ generate_colors.py
@@ -1,72 +1,430 @@
+colors=["#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+        "#FFC2E1", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+        "#5A0007", "#809693", "#FBFFB2", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+        "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+        "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+        "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+        "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+        "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+
+        "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+        "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+        "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
+        "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+        "#5B4534", "#FDC2C5", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C",
+        "#83AB58", "#001C1E", "#D1F7CE", "#004B28", "#C8D0F6", "#A3A489", "#806C66", "#222800",
+        "#BF5650", "#E83000", "#66796D", "#DA007C", "#FF1A59", "#8ADBB4", "#1E0200", "#5B4E51",
+        "#C895C5", "#320033", "#FF6832", "#66E1D3", "#CFCDAC", "#D0AC94", "#7ED379", "#012C58",
+
+        "#7A7BFF", "#D68E01", "#353339", "#78AFA1", "#FEB2C6", "#75797C", "#837393", "#943A4D",
+        "#B5F4FF", "#D2DCD5", "#9556BD", "#6A714A", "#001325", "#02525F", "#0AA3F7", "#E98176",
+        "#DBD5DD", "#5EBCD1", "#3D4F44", "#7E6405", "#02684E", "#962B75", "#8D8546", "#9695C5",
+        "#E773CE", "#D86A78", "#3E89BE", "#CA834E", "#518A87", "#5B113C", "#55813B", "#E704C4",
+        "#00005F", "#A97399", "#4B8160", "#59738A", "#FF5DA7", "#F7C9BF", "#643127", "#513A01",
+        "#6B94AA", "#51A058", "#A45B02", "#1D1702", "#E20027", "#E7AB63", "#4C6001", "#9C6966",
+        "#64547B", "#97979E", "#006A66", "#391406", "#F4D749", "#0045D2", "#006C31", "#DDB6D0",
+        "#7C6571", "#9FB2A4", "#00D891", "#15A08A", "#BC65E9", "#CEE1FF", "#C6DC99", "#203B3C",
+
+        "#671190", "#6B3A64", "#F5E1FF", "#FFA0F2", "#CCAA35", "#374527", "#8BB400", "#797868",
+        "#C6005A", "#3B000A", "#C86240", "#29607C", "#402334", "#7D5A44", "#CCB87C", "#B88183",
+        "#AA5199", "#B5D6C3", "#A38469", "#9F94F0", "#A74571", "#B894A6", "#71BB8C", "#00B433",
+        "#789EC9", "#6D80BA", "#953F00", "#5EFF03", "#E4FFFC", "#1BE177", "#BCB1E5", "#76912F",
+        "#003109", "#0060CD", "#D20096", "#895563", "#29201D", "#5B3213", "#A76F42", "#89412E",
+        "#1A3A2A", "#494B5A", "#A88C85", "#F4ABAA", "#A3F3AB", "#00C6C8", "#EA8B66", "#958A9F",
+        "#BDC9D2", "#9FA064", "#BE4700", "#658188", "#83A485", "#453C23", "#47675D", "#3A3F00",
+        "#061203", "#DFFB71", "#868E7E", "#98D058", "#6C8F7D", "#D7BFC2", "#3C3E6E", "#D83D66",
+
+        "#2F5D9B", "#6C5E46", "#D25B88", "#5B656C", "#00B57F", "#545C46", "#866097", "#365D25",
+        "#252F99", "#00CCFF", "#674E60", "#FC009C", "#92896B"]
+
+# colors = ["#B88183",
+# "#922329",
+# "#5A0007",
+# "#D7BFC2",
+# "#D86A78",
+# "#FF8A9A",
+# "#3B000A",
+# "#E20027",
+# "#943A4D",
+# "#5B4E51",
+# "#B05B6F",
+# "#FEB2C6",
+# "#D83D66",
+# "#895563",
+# "#FF1A59",
+# "#FFDBE5",
+# "#CC0744",
+# "#CB7E98",
+# "#997D87",
+# "#6A3A4C",
+# "#FF2F80",
+# "#6B002C",
+# "#A74571",
+# "#C6005A",
+# "#FF5DA7",
+# "#300018",
+# "#B894A6",
+# "#FF90C9",
+# "#7C6571",
+# "#A30059",
+# "#DA007C",
+# "#5B113C",
+# "#402334",
+# "#D157A0",
+# "#DDB6D0",
+# "#885578",
+# "#962B75",
+# "#A97399",
+# "#D20096",
+# "#E773CE",
+# "#AA5199",
+# "#E704C4",
+# "#6B3A64",
+# "#FFA0F2",
+# "#6F0062",
+# "#B903AA",
+# "#C895C5",
+# "#FF34FF",
+# "#320033",
+# "#DBD5DD",
+# "#EEC3FF",
+# "#BC23FF",
+# "#671190",
+# "#201625",
+# "#F5E1FF",
+# "#BC65E9",
+# "#D790FF",
+# "#72418F",
+# "#4A3B53",
+# "#9556BD",
+# "#B4A8BD",
+# "#7900D7",
+# "#A079BF",
+# "#958A9F",
+# "#837393",
+# "#64547B",
+# "#3A2465",
+# "#353339",
+# "#BCB1E5",
+# "#9F94F0",
+# "#9695C5",
+# "#0000A6",
+# "#000035",
+# "#636375",
+# "#00005F",
+# "#97979E",
+# "#7A7BFF",
+# "#3C3E6E",
+# "#6367A9",
+# "#494B5A",
+# "#3B5DFF",
+# "#C8D0F6",
+# "#6D80BA",
+# "#8FB0FF",
+# "#0045D2",
+# "#7A87A1",
+# "#324E72",
+# "#00489C",
+# "#0060CD",
+# "#789EC9",
+# "#012C58",
+# "#99ADC0",
+# "#001325",
+# "#DDEFFF",
+# "#59738A",
+# "#0086ED",
+# "#75797C",
+# "#BDC9D2",
+# "#3E89BE",
+# "#8CD0FF",
+# "#0AA3F7",
+# "#6B94AA",
+# "#29607C",
+# "#404E55",
+# "#006FA6",
+# "#013349",
+# "#0AA6D8",
+# "#658188",
+# "#5EBCD1",
+# "#456D75",
+# "#0089A3",
+# "#B5F4FF",
+# "#02525F",
+# "#1CE6FF",
+# "#001C1E",
+# "#203B3C",
+# "#A3C8C9",
+# "#00A6AA",
+# "#00C6C8",
+# "#006A66",
+# "#518A87",
+# "#E4FFFC",
+# "#66E1D3",
+# "#004D43",
+# "#809693",
+# "#15A08A",
+# "#00846F",
+# "#00C2A0",
+# "#00FECF",
+# "#78AFA1",
+# "#02684E",
+# "#C2FFED",
+# "#47675D",
+# "#00D891",
+# "#004B28",
+# "#8ADBB4",
+# "#0CBD66",
+# "#549E79",
+# "#1A3A2A",
+# "#6C8F7D",
+# "#008941",
+# "#63FFAC",
+# "#1BE177",
+# "#006C31",
+# "#B5D6C3",
+# "#3D4F44",
+# "#4B8160",
+# "#66796D",
+# "#71BB8C",
+# "#04F757",
+# "#001E09",
+# "#D2DCD5",
+# "#00B433",
+# "#9FB2A4",
+# "#003109",
+# "#A3F3AB",
+# "#456648",
+# "#51A058",
+# "#83A485",
+# "#7ED379",
+# "#D1F7CE",
+# "#A1C299",
+# "#061203",
+# "#1E6E00",
+# "#5EFF03",
+# "#55813B",
+# "#3B9700",
+# "#4FC601",
+# "#1B4400",
+# "#C2FF99",
+# "#788D66",
+# "#868E7E",
+# "#83AB58",
+# "#374527",
+# "#98D058",
+# "#C6DC99",
+# "#A4E804",
+# "#76912F",
+# "#8BB400",
+# "#34362D",
+# "#4C6001",
+# "#DFFB71",
+# "#6A714A",
+# "#222800",
+# "#6B7900",
+# "#3A3F00",
+# "#BEC459",
+# "#FEFFE6",
+# "#A3A489",
+# "#9FA064",
+# "#FFFF00",
+# "#61615A",
+# "#FFFFFE",
+# "#9B9700",
+# "#CFCDAC",
+# "#797868",
+# "#575329",
+# "#FFF69F",
+# "#8D8546",
+# "#F4D749",
+# "#7E6405",
+# "#1D1702",
+# "#CCAA35",
+# "#CCB87C",
+# "#453C23",
+# "#513A01",
+# "#FFB500",
+# "#A77500",
+# "#D68E01",
+# "#B79762",
+# "#7A4900",
+# "#372101",
+# "#886F4C",
+# "#A45B02",
+# "#E7AB63",
+# "#FAD09F",
+# "#C0B9B2",
+# "#938A81",
+# "#A38469",
+# "#D16100",
+# "#A76F42",
+# "#5B4534",
+# "#5B3213",
+# "#CA834E",
+# "#FF913F",
+# "#953F00",
+# "#D0AC94",
+# "#7D5A44",
+# "#BE4700",
+# "#FDE8DC",
+# "#772600",
+# "#A05837",
+# "#EA8B66",
+# "#391406",
+# "#FF6832",
+# "#C86240",
+# "#29201D",
+# "#B77B68",
+# "#806C66",
+# "#FFAA92",
+# "#89412E",
+# "#E83000",
+# "#A88C85",
+# "#F7C9BF",
+# "#643127",
+# "#E98176",
+# "#7B4F4B",
+# "#1E0200",
+# "#9C6966",
+# "#BF5650",
+# "#BA0900",
+# "#FF4A46",
+# "#F4ABAA",
+# "#000000",
+# "#452C2C",
+# "#C8A1A1"]
+
 import numpy as np
 import math
 import colorsys
+import struct
 
 num_colors = 256
-t = 5
+def colors1():
+    t = 20
 
-s_min = 0
-s_max = 1
+    s_min = 0.45
+    s_max = 0.55
 
-v_min = 0
-v_max = 1
+    v_min = 0.25
+    v_max = 1
 
-s_phase = 0
-v_phase = math.pi / 2
+    s_phase = 0
+    v_phase = math.pi / 2
 
+    # H: Generate indexes with 256 values that go between 0 and 360 (not included)
+    H = np.linspace(0, 1, num_colors, endpoint=False)
 
-# H: Generate indexes with 256 values that go between 0 and 360 (not included)
-H = np.linspace(0, 360, num_colors, endpoint=False)
-
-# S
-max_ = s_max
-min_ = s_min
-phase = s_phase
-S = (max_ - min_) * (np.sin((2 * math.pi * H / t) + phase) + 1) / 2.0 + min_
-
-
-# V
-max_ = v_max
-min_ = v_min
-phase = v_phase
-V = (max_ - min_) * (np.sin((2 * math.pi * H / t) + phase) + 1) / 2.0 + min_
+    # S
+    max_ = s_max
+    min_ = s_min
+    phase = s_phase
+    S = (max_ - min_) * (np.sin((2 * math.pi * H / t) + phase) + 1) / 2.0 + min_
 
 
-
-# HTML:
-html = """<!DOCTYPE html>
-<html>
-  <head>
-    <meta  content="text/html; charset=UTF-8"  http-equiv="content-type">
-    <title></title>
-  </head>
-  <body>
-    <table  style="height: 71px;"  border="1px solid">
-      <tbody>
-        <tr>
-          <td  style="width: 60px;">Id</td>
-          <td  style="width: 60px;">H</td>
-          <td  style="width: 60px;">S</td>
-          <td  style="width: 60px;">V</td>
-          <td  style="width: 123px;">Color</td>
-        </tr>"""
+    # V
+    max_ = v_max
+    min_ = v_min
+    phase = v_phase
+    V = (max_ - min_) * (np.sin((2 * math.pi * H / t) + phase) + 1) / 2.0 + min_
 
 
+    # HTML:
+    html = """<!DOCTYPE html>
+    <html>
+      <head>
+        <meta  content="text/html; charset=UTF-8"  http-equiv="content-type">
+        <title></title>
+      </head>
+      <body>
+        <table  style="height: 71px;"  border="1px solid">
+          <tbody>
+            <tr>
+              <td  style="width: 60px;">Id</td>
+              <td  style="width: 60px;">H</td>
+              <td  style="width: 60px;">S</td>
+              <td  style="width: 60px;">V</td>
+              <td  style="width: 123px;">Color</td>
+    </tr>"""
 
-for i in range(num_colors):
-    #color = colorsys.hsv_to_rgb(int(H[i]), int(S[i]), int(V[i]))
-    color = colorsys.hsv_to_rgb(H[i], S[i], V[i])
-    # print(int(color[0]*255), int(color[1]*255), int(color[2]*255))
-    html += "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td style='background-color: rgb({4},{5},{6})'><br></td>".format(
-        i, H[i], S[i], V[i], int(color[0]*255), int(color[1]*255), int(color[2]*255))
+    for i in range(num_colors):
+        color = colorsys.hsv_to_rgb(H[i], S[i], V[i])
+        html += "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td style='background-color: rgb({4},{5},{6})'><br></td>".format(
+            i, H[i], S[i], V[i], int(color[0]*255), int(color[1]*255), int(color[2]*255))
 
-html += "</table>"
+    html += "</table>"
 
-html += "<div>Range S: {0}-{1}, Phase: {2}</div>".format(s_min, s_max, s_phase)
-html += "<div>Range V: {0}-{1}, Phase: {2}</div>".format(v_min, v_max, v_phase)
-html += "<div>t = {0}</div>".format(t)
-html += "</body></html>"
+    html += "<div>Range S: {0}-{1}, Phase: {2}</div>".format(s_min, s_max, s_phase)
+    html += "<div>Range V: {0}-{1}, Phase: {2}</div>".format(v_min, v_max, v_phase)
+    html += "<div>t = {0}</div>".format(t)
+    html += "</body></html>"
 
-path = "colors-{0}-{1}-{2}-{3}-{4}-{5}-{6}.html".format(s_min, s_max, s_phase, v_min, v_max, v_phase, t)
-with open("/Users/jonieva/Desktop/" + path, 'w+b') as f:
-    f.write(html)
+    path = "colors-{0}-{1}-{2}-{3}-{4}-{5}-{6}.html".format(s_min, s_max, s_phase, v_min, v_max, v_phase, t)
+    with open("/Users/jonieva/Desktop/" + path, 'w+b') as f:
+        f.write(html)
+
+
+def colors2():
+    html = """<!DOCTYPE html>
+    <html>
+      <head>
+        <meta  content="text/html; charset=UTF-8"  http-equiv="content-type">
+        <title></title>
+      </head>
+      <body>
+        <table  style="height: 71px;"  border="1px solid">
+          <tbody>
+            <tr>
+              <td  style="width: 120px;">Color</td>
+              <td  style="width: 200px;">H</td>
+            """
+    for i in range(num_colors):
+        html += "<td style='border-width: 0px; width: 5px'></td>"
+
+    html += "</tr>"
+
+    S = np.linspace(0.35, 1, num_colors, endpoint=False)
+
+    for i in range(num_colors):
+        html +=  "<tr><td>{0}</td><td style='background-color: {0}; width: 200px'><br></td>".format(colors[i])
+        # Convert to RGB from hexadecimal
+        #col = struct.unpack('BBB', colors[i].replace("#", "").decode('hex'))
+        col = (int(colors[i][1:3], 16)/255.0, int(colors[i][3:5], 16)/255.0, int(colors[i][5:], 16)/255.0)
+        # print col
+        # Convert RGB to HLS
+        col_hls = colorsys.rgb_to_hls(col[0], col[1], col[2])
+        # print col
+        for i in range(num_colors):
+            # Keep the first 2 components and play with S value
+            col_rgb = colorsys.hls_to_rgb(col_hls[0], col_hls[1], S[i])
+            html += "<td style='background-color: rgb({0},{1},{2}); border: 0'><br></td>".format(int(col_rgb[0]*255), int(col_rgb[1]*255), int(col_rgb[2]*255))
+
+        html += "</tr>"
+        #html +=  "<tr><td>{0}</td><td></td><td></td><td></td><td style='background-color: {0}'><br></td>".format(colors[i])
+        # color = colorsys.hsv_to_rgb(H[i], S[i], V[i])
+        # html += "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td style='background-color: rgb({4},{5},{6})'><br></td>".format(
+        #     i, H[i], S[i], V[i], int(color[0]*255), int(color[1]*255), int(color[2]*255))
+
+    html += "</table>"
+
+    # html += "<div>Range S: {0}-{1}, Phase: {2}</div>".format(s_min, s_max, s_phase)
+    # html += "<div>Range V: {0}-{1}, Phase: {2}</div>".format(v_min, v_max, v_phase)
+    # html += "<div>t = {0}</div>".format(t)
+    html += "</body></html>"
+
+    #path = "colors-{0}-{1}-{2}-{3}-{4}-{5}-{6}.html".format(s_min, s_max, s_phase, v_min, v_max, v_phase, t)
+    path = "colors.html"
+    with open("/Users/jonieva/Desktop/" + path, 'w+b') as f:
+        f.write(html)
+
+def generate_slicer_palette(palette, output_path):
+    """ Generate an output file that will contain a palette based on the list of hexadecimal colors provided
+    :param palette:
+    :param output_path:
+    :return:
+    """
+    s = ""
+
+
+
+colors2()
